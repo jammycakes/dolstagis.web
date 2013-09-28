40bbf229851295da1599f1d22cdce5f3340342cf
James McKay <code@jamesmckay.net>
2013-09-28 12:08:36 +0100

    A module registers handlers, with routes and optionally route preconditions.

--- src/Dolstagis.Web/Module.cs
+++ src/Dolstagis.Web/Module.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dolstagis.Web.Routing;
 
 namespace Dolstagis.Web
 {
     public class Module
     {
-        public virtual string Description { get; }
+        public virtual string Description { get { return String.Empty; } }
 
         public bool Enabled { get; set; }
 
         public Module()
         {
             this.Enabled = true;
+        }
+
+        public void AddHandler<T>() where T: Handler
+        {
+        }
+
+        public void AddHandler<T>(string route) where T: Handler
+        {
+        }
+
+        public void AddHandler<T>(string route, Func<RouteInfo, bool> precondition) where T: Handler
+        {
         }
     }
 }
---
+++ src/Dolstagis.Web/RouteAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class RouteAttribute : Attribute
+    {
+        public string Route { get; private set; }
+
+        public RouteAttribute(string route)
+        {
+        }
+    }
+}
