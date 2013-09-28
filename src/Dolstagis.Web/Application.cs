4bcbe12c9eb9e81cfd2fdd4d0e95baacdc9886a1
James McKay <code@jamesmckay.net>
2013-09-28 12:53:17 +0100

    Add some documentation to the Application class and fix definition of AddModule.

--- src/Dolstagis.Web/Application.cs
+++ src/Dolstagis.Web/Application.cs
@@ -8,24 +8,74 @@ using StructureMap;
 
 namespace Dolstagis.Web
 {
+    /// <summary>
+    ///  The Application object is the singleton instance at the root of our
+    ///  application which marshals services and manages request lifecycles.
+    /// </summary>
+
     public class Application : IDisposable
     {
+        /// <summary>
+        ///  Called by the application container (an HTTP application, for example)
+        ///  to perform any setup tasks before requests can be processed.
+        /// </summary>
+
         public void Init()
         {
         }
 
-        public void AddModule<T>(T module) where T: Module, new()
+        /// <summary>
+        ///  Registers a module with the application by type.
+        /// </summary>
+        /// <typeparam name="T">
+        ///  They type of module to register.
+        /// </typeparam>
+
+        public void AddModule<T>() where T: Module, new()
+        {
+            AddModule(new T());
+        }
+
+        /// <summary>
+        ///  Registers a module with the application by instance.
+        /// </summary>
+        /// <param name="module">
+        ///  The module to register.
+        /// </param>
+
+        public void AddModule(Module module)
         {
         }
+
+        /// <summary>
+        ///  Processes a request synchronously.
+        /// </summary>
+        /// <param name="context">
+        ///  The <see cref="IHttpContext"/> containing request and response objects.
+        /// </param>
 
         public void ProcessRequest(IHttpContext context)
         {
             ProcessRequestAsync(context).Wait();
         }
 
+        /// <summary>
+        ///  Processes a request asynchronously.
+        /// </summary>
+        /// <param name="context">
+        ///  The <see cref="IHttpContext"/> containing request and response objects.
+        /// </param>
+        /// <returns>
+        ///  A <see cref="Task"/> instance.
+        /// </returns>
+
         public async Task ProcessRequestAsync(IHttpContext context)
         {
         }
+
+        /// <summary>
+        ///  Releases any resources when the application shuts down.
+        /// </summary>
 
         public void Dispose()
         {
