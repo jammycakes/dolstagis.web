2aabbb9e949480e4603617c202634f2055e72ec5
James McKay <code@jamesmckay.net>
2013-09-28 09:46:09 +0100

    This is how we will configure an ASP.NET application to use it

---
+++ src/Dolstagis.Web.Aspnet/HttpRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Dolstagis.Web.Aspnet
+{
+    public class HttpRequestHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+        }
+    }
+}
