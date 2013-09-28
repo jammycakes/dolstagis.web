fc04d222666f92211e701f85e2927b58166f72d5
James McKay <code@jamesmckay.net>
2013-09-28 14:02:44 +0100

    Add skeleton route definition classes.

---
+++ src/Dolstagis.Web/Routing/IRouteDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Routing
+{
+    /// <summary>
+    ///  Represents an entry in the route table.
+    /// </summary>
+
+    public interface IRouteDefinition
+    {
+        /// <summary>
+        ///  Gets the type of handler which will be instantiated by this route.
+        /// </summary>
+
+        Type HandlerType { get; }
+
+        /// <summary>
+        ///  Verifies that the preconditions for this route have been met.
+        /// </summary>
+        /// <param name="routeInfo"></param>
+        /// <returns></returns>
+
+        bool IsPreconditionMet(RouteInfo routeInfo);
+
+        /// <summary>
+        ///  Gets the module in which this route was declared.
+        /// </summary>
+
+        Module Module { get; }
+    }
+}
---
+++ src/Dolstagis.Web/Routing/RouteDefinition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Routing
+{
+    public class RouteDefinition : IRouteDefinition
+    {
+        private Func<RouteInfo, bool> _precondition;
+
+        public RouteDefinition(Type handlerType, string route, Module module, Func<RouteInfo, bool> precondition)
+        {
+            this.HandlerType = handlerType;
+            this.Route = route;
+            this.Module = module;
+            this._precondition = precondition;
+        }
+
+        public Type HandlerType { get; private set; }
+
+        public string Route { get; private set; }
+
+        public bool IsPreconditionMet(RouteInfo routeInfo)
+        {
+            return _precondition(routeInfo);
+        }
+
+        public Module Module { get; private set; }
+    }
+}
