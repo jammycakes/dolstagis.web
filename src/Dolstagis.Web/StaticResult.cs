6cd0061c101f6867b2484f4fdc18fba8eb90dd0e
James McKay <code@jamesmckay.net>
2013-10-01 17:49:10 +0100

    StaticResult and ViewResult classes.

---
+++ src/Dolstagis.Web/StaticResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dolstagis.Web.Routing;
+
+namespace Dolstagis.Web
+{
+    public class StaticResult
+    {
+        public string Path { get; private set; }
+
+        public StaticResult(string path)
+        {
+            this.Path = path.NormaliseUrlPath();
+        }
+    }
+}
---
+++ src/Dolstagis.Web/ViewResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web
+{
+    public class ViewResult
+    {
+        public string Path { get; private set; }
+
+        public object Data { get; private set; }
+
+        public ViewResult(string path)
+        {
+            this.Path = path;
+            this.Data = null;
+        }
+
+        public ViewResult(string path, object data)
+        {
+            this.Path = path;
+            this.Data = data;
+        }
+    }
+}
