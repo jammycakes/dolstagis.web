3d85be588b14bddbf54fed5367952e7ada04ce60
James McKay <code@jamesmckay.net>
2013-10-01 17:51:22 +0100

    Move IViewFactory to Views namespace.

--- src/Dolstagis.Web/Views/IViewFactory.cs
+++ src/Dolstagis.Web/Views/IViewFactory.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Dolstagis.Web.Views;
 
-namespace Dolstagis.Web.Lifecycle
+namespace Dolstagis.Web.Views
 {
     public interface IViewFactory
     {
