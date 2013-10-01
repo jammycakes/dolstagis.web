4372b6ad808eb15ff81e22e488deca3587329c20
James McKay <code@jamesmckay.net>
2013-10-01 17:41:57 +0100

    View engine and view engine registry stub

---
+++ src/Dolstagis.Web/Views/ViewEngineRegistry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Views
+{
+    public class ViewEngineRegistry
+    {
+        private IDictionary<string, IViewEngine> _viewEngines
+            = new Dictionary<string, IViewEngine>(StringComparer.OrdinalIgnoreCase);
+
+        public ViewEngineRegistry(IEnumerable<IViewEngine> viewEngines)
+        {
+            foreach (var engine in viewEngines)
+                foreach (var ext in engine.Extensions)
+                    _viewEngines[ext] = engine;
+        }
+    }
+}
