fd85b42549022d30755b8a4e871a1ea8819fee41
James McKay <code@jamesmckay.net>
2013-10-03 22:20:08 +0100

    IRequestContextBuilder class.

---
+++ src/Dolstagis.Web/Lifecycle/RequestContextBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Lifecycle
+{
+    public class RequestContextBuilder : IRequestContextBuilder
+    {
+        public IRequestContext CreateContext(Http.IRequest request, Http.IResponse response)
+        {
+            return new RequestContext(request, response);
+        }
+    }
+}
