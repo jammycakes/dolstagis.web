15f5356e29a26799392f1dc016b241157d17a1b2
James McKay <code@jamesmckay.net>
2013-10-04 20:27:29 +0100

    Created a VirtualPath class so we know exactly what we're dealing with.

---
+++ src/Dolstagis.Tests/Web/VirtualPathFixture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dolstagis.Web;
+using NUnit.Framework;
+
+namespace Dolstagis.Tests.Web
+{
+    [TestFixture]
+    public class VirtualPathFixture
+    {
+        [TestCase("/one/two", VirtualPathType.Absolute, "one/two")]
+        [TestCase("~/one/two", VirtualPathType.AppRelative, "one/two")]
+        [TestCase("one/two", VirtualPathType.RequestRelative, "one/two")]
+        [TestCase("../one/two", VirtualPathType.RequestRelative, "../one/two")]
+        [TestCase("../one/../two", VirtualPathType.RequestRelative, "../two")]
+        [TestCase("../one/../../two", VirtualPathType.RequestRelative, "../../two")]
+        [TestCase("~/../one/two", VirtualPathType.AppRelative, "../one/two")]
+        [TestCase("/one/two/", VirtualPathType.Absolute, "one/two")]
+        public void CanDetectPathType(string path, VirtualPathType type, string expectedPath)
+        {
+            var vpath = new VirtualPath(path);
+            Assert.AreEqual(type, vpath.Type);
+            Assert.AreEqual(expectedPath, vpath.Path);
+        }
+
+        [TestCase("/one/two", "three/four", VirtualPathType.Absolute, "one/two/three/four")]
+        [TestCase("/one/two", "/three/four", VirtualPathType.Absolute, "three/four")]
+        [TestCase("one/two", "three/four", VirtualPathType.RequestRelative, "one/two/three/four")]
+        [TestCase("one/two", "/three/four", VirtualPathType.Absolute, "three/four")]
+        [TestCase("one/two", "../../../three/four", VirtualPathType.RequestRelative, "../three/four")]
+        [TestCase("/one/two", "../../../three/four", VirtualPathType.Absolute, "three/four")]
+        public void CanAppend(string first, string second, VirtualPathType expectedType, string expectedPath)
+        {
+            var v1 = new VirtualPath(first);
+            var v2 = new VirtualPath(second);
+            var actual = v1.Append(v2);
+            Assert.AreEqual(expectedType, actual.Type);
+            Assert.AreEqual(expectedPath, actual.Path);
+        }
+    }
+}
---
+++ src/Dolstagis.Web/VirtualPath.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web
+{
+    /// <summary>
+    ///  Rw
+    /// </summary>
+
+    public class VirtualPath
+    {
+        public VirtualPathType Type { get; private set; }
+
+        public string Path { get; private set; }
+
+        public IList<string> Parts { get; private set; }
+
+        public VirtualPath(string path)
+        {
+            if (path.StartsWith("/")) {
+                Type = VirtualPathType.Absolute;
+                path = path.Substring(1);
+            }
+            else if (path.StartsWith("~/")) {
+                Type = VirtualPathType.AppRelative;
+                path = path.Substring(2);
+            }
+            else {
+                Type = VirtualPathType.RequestRelative;
+            }
+
+            Parts = GetParts(path).ToList().AsReadOnly();
+            Path = String.Join("/", Parts);
+        }
+
+        public VirtualPath(IEnumerable<string> parts, VirtualPathType type)
+        {
+            Type = type;
+            Parts = GetParts(parts).ToList().AsReadOnly();
+            Path = String.Join("/", Parts);
+        }
+
+        private IEnumerable<string> GetParts(string path)
+        {
+            return GetParts(path.Trim('/').Split('/'));
+        }
+
+        private IEnumerable<string> GetParts(IEnumerable<string> parts)
+        {
+            var stack = new Stack<string>();
+
+            foreach (var part in parts) {
+                if (stack.Count == 0) {
+                    stack.Push(part);
+                }
+                else if (part == ".." && stack.Peek() != "..") {
+                    stack.Pop();
+                }
+                else {
+                    stack.Push(part);
+                }
+            }
+            var result = stack.Reverse();
+            if (Type == VirtualPathType.Absolute) {
+                result = result.SkipWhile(x => x == "..");
+            }
+            return result;
+        }
+
+        public VirtualPath Append(VirtualPath other)
+        {
+            if (other.Type != VirtualPathType.RequestRelative) {
+                return other;
+            }
+            else {
+                return new VirtualPath(this.Parts.Concat(other.Parts), this.Type);
+            }
+        }
+    }
+}
