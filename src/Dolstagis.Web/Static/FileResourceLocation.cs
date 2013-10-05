9ebd14b29c70994141bbd0ef23528f81143becf7
James McKay <code@jamesmckay.net>
2013-10-05 14:38:19 +0100

    ResourceLocation and FileResourceLocation

---
+++ src/Dolstagis.Web/Static/FileResourceLocation.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Static
+{
+    public class FileResourceLocation : ResourceLocation
+    {
+        public string FileLocation { get; private set; }
+
+        /// <summary>
+        ///  Creates a new <see cref="FileResourceLocation"/> serving up files from a
+        ///  virtual path within the application root directory.
+        /// </summary>
+        /// <param name="root">
+        ///  The root URL for the files. This must be app-relative.
+        /// </param>
+        /// <param name="application"></param>
+
+        public FileResourceLocation(VirtualPath root, IApplicationContext application)
+            : base(root)
+        {
+            var parts = new string[] { application.PhysicalPath }.Concat(root.Parts).ToArray();
+            FileLocation = Path.Combine(parts);
+        }
+
+        /// <summary>
+        ///  Creates a new <see cref="FileResourceLocation"/> serving up files from a
+        ///  physical path at an arbitrary location on the filesystem.
+        /// </summary>
+        /// <param name="root">
+        ///  The root URL for the files. This must be app-relative.
+        /// </param>
+        /// <param name="fileLocation">
+        ///  The location in the filespace where the files are located.
+        /// </param>
+
+        public FileResourceLocation(VirtualPath root, string fileLocation)
+            : base(root)
+        {
+            FileLocation = fileLocation;
+        }
+
+        /// <summary>
+        ///  Creates a new <see cref="FileResourceLocation"/> serving up files from a
+        ///  virtual path within the application root directory, but at a different URL.
+        /// </summary>
+        /// <param name="root">
+        ///  The root URL for the files. This must be app-relative.
+        /// </param>
+        /// <param name="fileLocation">
+        ///  The location in the application where the files are located. This may be either
+        ///  app-relative or request-relative; in the latter case, it will be taken as being
+        ///  relative to the root.
+        /// </param>
+        /// <param name="application"></param>
+
+        public FileResourceLocation(VirtualPath root, VirtualPath fileLocation, IApplicationContext application)
+            : base(root)
+        {
+            switch (fileLocation.Type) {
+                case VirtualPathType.Absolute:
+                    throw new ArgumentException("The file location must be within the application.", "fileLocation");
+                case VirtualPathType.RequestRelative:
+                    fileLocation = root.Append(fileLocation);
+                    break;
+            }
+
+            var parts = new string[] { application.PhysicalPath }.Concat(fileLocation.Parts).ToArray();
+            FileLocation = Path.Combine(parts);
+        }
+
+        protected override IResource CreateResource(VirtualPath path)
+        {
+            var parts = new string[] { FileLocation }.Concat(path.Parts).ToArray();
+            var physicalPath = Path.Combine(parts);
+            if (File.Exists(physicalPath)) {
+                return new FileResource(physicalPath);
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/Dolstagis.Web/Static/ResourceLocation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Static
+{
+    public abstract class ResourceLocation
+    {
+        public VirtualPath Root { get; private set; }
+
+        protected abstract IResource CreateResource(VirtualPath path);
+
+        protected ResourceLocation(VirtualPath root)
+        {
+            if (root.Type != VirtualPathType.AppRelative) {
+                throw new ArgumentException("The root path must be app-relative.");
+            }
+            Root = root;
+        }
+
+        public IResource GetResource(VirtualPath path)
+        {
+            switch (path.Type) {
+                case VirtualPathType.Absolute:
+                    throw new ArgumentException("The path to locate must be app-relative.");
+                case VirtualPathType.AppRelative:
+                    path = Root.GetSubPath(path, true);
+                    if (path == null) {
+                        throw new ArgumentException("The path to locate must be a sub-path of the root.");
+                    }
+                    break;
+            }
+            if (path.Parts.FirstOrDefault() == "..") {
+                throw new ArgumentException("The path to locate must be a sub-path of the root.");
+            }
+
+            return CreateResource(path);
+        }
+    }
+}
