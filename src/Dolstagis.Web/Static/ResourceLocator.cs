5fd30dc5b4484119113b206b8f64b10746c44438
James McKay <code@jamesmckay.net>
2013-10-05 18:07:44 +0100

    New ResourceLocator class.

---
+++ src/Dolstagis.Web/Static/ResourceLocator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Static
+{
+    public class ResourceLocator
+    {
+        private IEnumerable<ResourceLocation> _locations;
+
+        public ResourceLocator(IEnumerable<ResourceLocation> locations)
+        {
+            _locations = locations;
+        }
+
+        public IResource GetResource(VirtualPath path)
+        {
+            var candidates =
+                from location in _locations
+                let mine = location.Root.GetSubPath(path, true)
+                orderby location.Root.Parts.Count descending
+                let resource = location.GetResource(mine)
+                where resource != null
+                select resource;
+
+            return candidates.FirstOrDefault();
+        }
+    }
+}
