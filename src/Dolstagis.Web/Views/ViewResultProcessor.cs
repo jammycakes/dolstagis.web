9e23a9940e62c6fd13afd707ac328d003c9d3691
James McKay <code@jamesmckay.net>
2013-10-06 18:25:02 +0100

    ViewResultProcessor base class, stub for

---
+++ src/Dolstagis.Web/Views/ViewResultProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dolstagis.Web.Lifecycle;
+
+namespace Dolstagis.Web.Views
+{
+    public class ViewResultProcessor : ResultProcessor<ViewResult>
+    {
+        private ViewEngineRegistry _registry;
+
+        public ViewResultProcessor(ViewEngineRegistry registry)
+        {
+            _registry = registry;
+        }
+
+        public override Task Process(ViewResult data, IRequestContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
