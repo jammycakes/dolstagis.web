24cef51438a6833389edb44f2567789d3476c6e4
James McKay <code@jamesmckay.net>
2013-10-22 18:07:18 +0100

    Some helper methods in the Handler base class to return view results.

--- src/Dolstagis.Web/Handler.cs
+++ src/Dolstagis.Web/Handler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dolstagis.Web.Static;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,56 @@ namespace Dolstagis.Web
     public class Handler
     {
         public IRequestContext Context { get; internal set; }
+
+        /// <summary>
+        ///  Returns a static content result.
+        /// </summary>
+        /// <param name="path">
+        ///  The path to the static content.
+        /// </param>
+        /// <returns></returns>
+        public ResultBase Static(string path)
+        {
+            return new StaticResult(new VirtualPath(path));
+        }
+
+        /// <summary>
+        ///  Returns a static content result.
+        /// </summary>
+        /// <param name="path">
+        ///  The path to the static content.
+        /// </param>
+        /// <returns></returns>
+        public ResultBase Static(VirtualPath path)
+        {
+            return new StaticResult(path);
+        }
+
+        /// <summary>
+        ///  Returns a view result.
+        /// </summary>
+        /// <param name="path">
+        ///  The path to the view template.
+        /// </param>
+        /// <returns></returns>
+        public ResultBase View(string path)
+        {
+            return new ViewResult(path);
+        }
+
+        /// <summary>
+        ///  Returns a view result with a model.
+        /// </summary>
+        /// <param name="path">
+        ///  The path to the view result.
+        /// </param>
+        /// <param name="model">
+        ///  The model.
+        /// </param>
+        /// <returns></returns>
+        public ResultBase View(string path, object model)
+        {
+            return new ViewResult(path, model);
+        }
     }
 }
