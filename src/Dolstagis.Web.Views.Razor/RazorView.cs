1f2aa08dc8969b27680d9aa567d295df062034ab
James McKay <code@jamesmckay.net>
2013-10-29 18:02:32 +0000

    Stubs for RazorView and RazorViewEngine.

---
+++ src/Dolstagis.Web.Views.Razor/RazorView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dolstagis.Web.Static;
+
+namespace Dolstagis.Web.Views.Razor
+{
+    public class RazorView : IView
+    {
+        private RazorViewEngine _engine;
+        private VirtualPath _pathToView;
+        private IResource _resource;
+        private IResourceResolver _resolver;
+
+        public RazorView(RazorViewEngine razorViewEngine, VirtualPath pathToView, Static.IResource resource, Static.IResourceResolver resolver)
+        {
+            this._engine = razorViewEngine;
+            this._pathToView = pathToView;
+            this._resource = resource;
+            this._resolver = resolver;
+        }
+        public async Task Render(Stream stream, ViewResult data)
+        {
+        }
+    }
+}
---
+++ src/Dolstagis.Web.Views.Razor/RazorViewEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Views.Razor
+{
+    public class RazorViewEngine : ViewEngineBase
+    {
+        private static readonly string[] _extensions = new[] { "cshtml", "vbhtml" };
+
+        public override IEnumerable<string> Extensions
+        {
+            get { return _extensions; }
+        }
+
+        protected override IView CreateView(VirtualPath pathToView, Static.IResourceResolver resolver)
+        {
+            var resource = resolver.GetResource(pathToView);
+            if (resource == null || !resource.Exists)
+            {
+                throw new ViewNotFoundException("There is no view at " + pathToView.ToString());
+            }
+
+            return new RazorView(this, pathToView, resource, resolver);
+        }
+    }
+}
