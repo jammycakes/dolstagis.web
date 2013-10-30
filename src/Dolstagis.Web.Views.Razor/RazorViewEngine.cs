6c9af8fc2df23bea60243864b9fbbf1fc5283a3b
James McKay <code@jamesmckay.net>
2013-10-30 17:25:46 +0000

    Razor: compiled template base class and some basic settings

--- src/Dolstagis.Web.Views.Razor/RazorViewEngine.cs
+++ src/Dolstagis.Web.Views.Razor/RazorViewEngine.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Razor;
+using System.Web.Razor.Generator;
 using Dolstagis.Web.Static;
 
 namespace Dolstagis.Web.Views.Razor
@@ -12,11 +13,31 @@ namespace Dolstagis.Web.Views.Razor
     {
         private readonly string _extension;
         private readonly RazorCodeLanguage _language;
+        private readonly RazorEngineHost _host;
 
         public RazorViewEngine(string extension, RazorCodeLanguage language)
         {
             _extension = extension;
             _language = language;
+            _host = new RazorEngineHost(language)
+            {
+                DefaultBaseClass = typeof(TemplateBase).FullName,
+                DefaultNamespace = "Dolstagis.Web.Views.Razor.Compiled",
+                GeneratedClassContext = new GeneratedClassContext(
+                    executeMethodName: "Execute",
+                    writeMethodName: "Write",
+                    writeLiteralMethodName: "WriteLiteral",
+                    writeToMethodName: "WriteTo",
+                    writeLiteralToMethodName: "WriteLiteralTo",
+                    templateTypeName: null,
+                    defineSectionMethodName: "DefineSection"
+                )
+            };
+            _host.NamespaceImports.Add("System");
+            _host.NamespaceImports.Add("System.Collections.Generic");
+            _host.NamespaceImports.Add("System.Linq");
+            _host.NamespaceImports.Add("System.Text");
+            _host.NamespaceImports.Add("System.Threading.Tasks");
         }
 
         public override IEnumerable<string> Extensions
