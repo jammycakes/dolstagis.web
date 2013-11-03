27f7f49cd0824f3ed03f82bf80b3305914f3dc14
James McKay <code@jamesmckay.net>
2013-11-03 00:47:20 +0000

    Default error page when none are defined.

--- src/Dolstagis.Web/Lifecycle/ExceptionHandler.cs
+++ src/Dolstagis.Web/Lifecycle/ExceptionHandler.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dolstagis.Web.Http;
 using Dolstagis.Web.Static;
 using Dolstagis.Web.Views;
 
@@ -12,10 +13,12 @@ namespace Dolstagis.Web.Lifecycle
     public class ExceptionHandler : IExceptionHandler
     {
         private ViewRegistry _viewRegistry;
+        private ISettings _settings;
 
-        public ExceptionHandler(ViewRegistry viewRegistry)
+        public ExceptionHandler(ViewRegistry viewRegistry, ISettings settings)
         {
             _viewRegistry = viewRegistry;
+            _settings = settings;
         }
 
         public async Task HandleException(IRequestContext context, Exception ex)
@@ -30,19 +33,41 @@ namespace Dolstagis.Web.Lifecycle
             }
         }
 
-        private static async Task DumpException(IRequestContext context, Exception ex)
+        private async Task DumpException(IRequestContext context, HttpStatusException ex)
         {
-            context.Response.AddHeader("Content-Type", "text/plain");
+            context.Response.Status = ex.Status;
+            context.Response.AddHeader("Content-Type", "text/html");
             context.Response.AddHeader("Content-Encoding", "utf-8");
+
+            const string rn = "Dolstagis.Web.Errors.DefaultErrorPage.html";
+            string template;
+
+            using (var stream = this.GetType().Assembly.GetManifestResourceStream(rn))
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                template = await reader.ReadToEndAsync();
+            }
+
+            var html = template
+                .Replace("{{code}}", ex.Status.Code.ToString())
+                .Replace("{{title}}", HttpUtility.HtmlEncode(ex.Status.Description))
+                .Replace("{{description}}", HttpUtility.HtmlEncode(ex.Status.Message))
+                .Replace("{{debug}}", _settings.Debug ? RenderDebugInfo(context, ex) : "");
+
             using (var writer = new StreamWriter(context.Response.ResponseStream, Encoding.UTF8))
             {
-                await writer.WriteLineAsync(ex.ToString());
+                await writer.WriteAsync(html);
             }
+        }
+
+        private string RenderDebugInfo(IRequestContext context, HttpStatusException ex)
+        {
+            return "";
         }
 
         private async Task HandleHttpStatusException(IRequestContext context, HttpStatusException ex)
         {
-            Exception fault = null;
+            HttpStatusException fault = null;
 
             var vPath = new VirtualPath("~/errors/" + ex.Status.Code);
             var view = _viewRegistry.GetView(vPath) ??
@@ -59,7 +84,7 @@ namespace Dolstagis.Web.Lifecycle
                 }
                 catch (Exception exRendering)
                 {
-                    fault = new AggregateException(ex, exRendering);
+                    fault = new HttpStatusException(new AggregateException(ex, exRendering));
                     context.Response.Status = Status.InternalServerError;
                 }
             }
