a3a840a5241ffc6732b731a4a4247dd8623d6622
James McKay <code@jamesmckay.net>
2013-11-04 00:21:18 +0000

    Get build version info from command line if specified.

--- build.py
+++ build.py
@@ -1,12 +1,21 @@
 import os.path
 from buildlib import *
+import argparse
+import sys
+
+argparser = argparse.ArgumentParser(description = 'Build Dolstagis.Web')
+argparser.add_argument('configuration', default = 'Debug', nargs = '?')
+argparser.add_argument('-v', '--version', default = '0.1.0')
+argparser.add_argument('-i', '--versioninfo', default = 'alpha')
+argparser.add_argument('-n', '--build', default=0, type=int)
+args = argparser.parse_args(sys.argv[1:])
 
 project = Project(__file__, 'build')
 
-project.version = '0.0.3'
-project.versioninfo = 'alpha'
-project.build_number = 0
-project.configuration = 'Debug'
+project.version = args.version
+project.versioninfo = args.versioninfo
+project.build_number = args.build
+project.configuration = args.configuration
 
 project.start()
 project.clean()
@@ -19,4 +28,3 @@ project.make_nuget('Dolstagis.Web.Aspnet',
     content = ['web.config.transform']
 )
 project.make_nuget('Dolstagis.Web.Views.Nustache')
-
