5ec74b7b4eefeebd7720d2ed39cd7d5b5e3228e6
James McKay <code@jamesmckay.net>
2013-11-11 17:38:08 +0000

    Authentication attributes.

---
+++ src/Dolstagis.Web/Auth/RequireLoginAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.Auth
+{
+    /// <summary>
+    ///  Requires the user to be logged in.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class RequireLoginAttribute : Attribute, IRequirement
+    {
+        public bool IsDenied(IHttpContext context)
+        {
+            return context.User == null;
+        }
+    }
+}
