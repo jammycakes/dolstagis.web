10d702aa384de83cb0824a09bc73deb16e326aa1
James McKay <code@jamesmckay.net>
2013-11-26 08:25:30 +0000

    Our IHtmlString implementation.

---
+++ src/Dolstagis.Web/HtmlString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web
+{
+    public class HtmlString : IHtmlString
+    {
+        private string _content;
+
+        public HtmlString(string content)
+        {
+            _content = content;
+        }
+
+        public string ToHtmlString()
+        {
+            return _content;
+        }
+
+        public override string ToString()
+        {
+            return _content;
+        }
+    }
+}
