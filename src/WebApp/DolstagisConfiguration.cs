e5693da49fe77f0d3a47d6809bb0b093d90440c8
James McKay <code@jamesmckay.net>
2014-09-09 18:53:15 +0100

    Wire up OWIN-based configuration in sample front end app.

---
+++ src/WebApp/DolstagisConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dolstagis.Web.Aspnet;
+
+namespace WebApp
+{
+    public class DolstagisConfiguration : IConfigurator
+    {
+        public void Configure(Dolstagis.Web.Application application)
+        {
+            application.ScanForFeatures();
+        }
+    }
+}
