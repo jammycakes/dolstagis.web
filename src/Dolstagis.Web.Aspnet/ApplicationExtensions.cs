821c7c10a3c954bc6c7802600dd91b8b5ccbca3c
James McKay <code@jamesmckay.net>
2014-09-09 18:51:50 +0100

    Add new OWIN-based infrastructure for ASP.NET-hosted option.

---
+++ src/Dolstagis.Web.Aspnet/ApplicationExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Compilation;
+
+namespace Dolstagis.Web.Aspnet
+{
+    public static class ApplicationExtensions
+    {
+        static readonly string[] _ignores = new string[] {
+            "Microsoft.",
+            "mscorlib,",
+            "System.",
+            "System,",
+            "IronPython",
+            "IronRuby",
+            "CR_ExtUnitTest",
+            "CR_VSTest",
+            "DevExpress.CodeRush",
+            "StructureMap,",
+            "Nustache.Core,",
+            "Newtonsoft.Json,",
+            "Dolstagis.Web,"
+        };
+
+        internal static IEnumerable<Assembly> FindAssemblies(this Application application)
+        {
+            var assemblies =
+                from assembly in BuildManager.GetReferencedAssemblies().Cast<Assembly>()
+                where !assembly.IsDynamic
+                where !_ignores.Any(x => assembly.FullName.StartsWith(x, StringComparison.InvariantCulture))
+                let order =
+                    assembly.FullName.StartsWith("Dolstagis.Web,", StringComparison.InvariantCulture) ? 0 :
+                    assembly.FullName.StartsWith("Dolstagis.Web.") ? 1 :
+                    2
+                orderby order
+                select assembly;
+            return assemblies;
+        }
+
+
+        public static void ScanForFeatures(this Application application)
+        {
+            foreach (var assembly in FindAssemblies(application)) {
+                application.AddAllFeaturesInAssembly(assembly);
+            }
+        }
+    }
+}
---
+++ src/Dolstagis.Web.Aspnet/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Reflection;
+using Dolstagis.Web.Util;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Dolstagis.Web.Aspnet.Startup))]
+
+namespace Dolstagis.Web.Aspnet
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    using MidFunc = Func<
+            Func<IDictionary<string, object>, Task>,
+            Func<IDictionary<string, object>, Task>
+        >;
+
+    public class Startup
+    {
+        /*
+         * Adding this dummy reference to the OwinHttpHandler type ensures that
+         * Microsoft.Owin.Host.SystemWeb will always be included in the build of
+         * your web application, even if it isn't explicitly referenced by your
+         * front end web project itself.
+         * 
+         * This is necessary to ensure that the Owin infrastructure gets loaded
+         * by System.Web in the first place.
+         */
+
+        private static readonly object dummy = typeof(Microsoft.Owin.Host.SystemWeb.OwinHttpHandler);
+
+        public void Configuration(IAppBuilder app)
+        {
+            var application = new Application(new Settings());
+
+            var assemblies = application.FindAssemblies();
+            foreach (var assembly in assemblies)
+                foreach (var conf in assembly.SafeGetInstances<IConfigurator>())
+                    conf.Configure(application);
+
+            MidFunc middleware = next => application.GetAppFunc();
+            app.Use(middleware);
+        }
+    }
+}
