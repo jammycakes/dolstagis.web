e89f75f543523e7a4eec19d00931a49f85f81b5f
James McKay <code@jamesmckay.net>
2014-09-11 23:12:46 +0100

    Remove precondition from routes as it's not been implemented.

--- src/Dolstagis.Web/Routing/RouteDefinition.cs
+++ src/Dolstagis.Web/Routing/RouteDefinition.cs
@@ -8,24 +8,16 @@ namespace Dolstagis.Web.Routing
 {
     public class RouteDefinition : IRouteDefinition
     {
-        private Func<RouteInfo, bool> _precondition;
-
         public RouteDefinition(Type handlerType, string route, IRouteRegistry feature, Func<RouteInfo, bool> precondition)
         {
             this.HandlerType = handlerType;
             this.Route = route;
             this.Feature = feature;
-            this._precondition = precondition;
         }
 
         public Type HandlerType { get; private set; }
 
         public string Route { get; private set; }
-
-        public bool IsPreconditionMet(RouteInfo routeInfo)
-        {
-            return _precondition(routeInfo);
-        }
 
         public IRouteRegistry Feature { get; private set; }
     }
