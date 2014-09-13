d202efa0cea58b8483b1c2db2d28d60f90f44b13
James McKay <code@jamesmckay.net>
2014-09-13 12:22:24 +0100

    Add a new simplified routing engine for use with the new feature architecture.
    Instead of a single, centralised routing engine, we will instead give each
    feature its own route table. This means that individual features can specify
    their own routing engines if they so wish.
    
    This new routing engine is also simpler because it is decoupled from the feature
    class itself. The tests also only test the interface to the routing engine,
    whereas with the old version they were unnecessarily concerned with its internal
    mechanism.

---
+++ src/Dolstagis.Tests/Web/Routes/RouteTableFixture.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dolstagis.Web;
+using Dolstagis.Web.Routes;
+using Moq;
+using NUnit.Framework;
+
+namespace Dolstagis.Tests.Web.Routes
+{
+    [TestFixture]
+    public class RouteTableFixture
+    {
+        private RouteTable routes = new RouteTable();
+        private IRouteTarget rootTarget = Mock.Of<IRouteTarget>();
+        private IRouteTarget oneTwo = Mock.Of<IRouteTarget>();
+        private IRouteTarget oneThree = Mock.Of<IRouteTarget>();
+        private IRouteTarget withId = Mock.Of<IRouteTarget>();
+        private IRouteTarget withGreedyId = Mock.Of<IRouteTarget>();
+        private IRouteTarget withOptionalId = Mock.Of<IRouteTarget>();
+        private IRouteTarget withOptGreedyId = Mock.Of<IRouteTarget>();
+        private IRouteTarget withLanguage = Mock.Of<IRouteTarget>();
+
+
+        [TestFixtureSetUp]
+        public void CreateRouteTable()
+        {
+            routes.Add(String.Empty, rootTarget, req => true);
+
+            routes.Add("one/two", oneTwo, req => true);
+            routes.Add("one/three", oneThree, req => true);
+            routes.Add("one/two/three/{id}", withId, req => true);
+            routes.Add("one/two/greedy/{id+}", withGreedyId, req => true);
+            routes.Add("one/two/optional/{id?}", withOptionalId, req => true);
+            routes.Add("one/two/optgreedy/{id*}", withOptGreedyId, req => true);
+            routes.Add("{id}/one/two", withLanguage, req => true);
+        }
+
+
+        /* ====== CanGetMatchingRouteData ====== */
+
+        /*
+         * First check that valid routes match and give the correct parameters
+         */
+
+        [TestCase("~/", null)]
+        [TestCase("~/one/two", null)]
+        [TestCase("~/one/three", null)]
+        [TestCase("~/one/two/three/four", "four")]
+        [TestCase("~/one/two/greedy/four", "four")]
+        [TestCase("~/one/two/greedy/four/five/six", "four/five/six")]
+        [TestCase("~/one/two/optional", null)]
+        [TestCase("~/one/two/optional/four", "four")]
+        [TestCase("~/one/two/optgreedy", null)]
+        [TestCase("~/one/two/optgreedy/four", "four")]
+        [TestCase("~/one/two/optgreedy/four/five/six", "four/five/six")]
+        [TestCase("~/en-gb/one/two", "en-gb")]
+        public void CanGetMatchingRouteData(string path, string expectedId)
+        {
+            var invocation = routes.GetRouteInvocation(new VirtualPath(path));
+            object id;
+            if (!invocation.RouteData.TryGetValue("id", out id)) id = null;
+            Assert.AreEqual(expectedId, id);
+        }
+
+
+        /* ====== DoesNotGetNonMatchingRouteData ====== */
+
+        /*
+         * Now test some routes that should not match.
+         */
+
+        [TestCase("~/one")]                     // intermediate route with no target
+        [TestCase("~/one/four")]                // nonexistent route
+        [TestCase("~/zero/four")]               // nonexistent route
+        [TestCase("~/one/two/three")]           // required route data is missing
+        [TestCase("~/one/two/three/four/five")] // required nongreedy has too many parts
+        [TestCase("~/one/two/greedy")]          // required greedy route data is missing
+        [TestCase("~/one/two/optional/3/4/5")]  // optional nongreedy has too many parts
+        [TestCase("~/en-gb/one/three/")]        // initial parameter but the rest doesn't match
+        public void DoesNotGetNonMatchingRouteData(string path)
+        {
+            var target = routes.GetRouteInvocation(new VirtualPath(path));
+            Assert.IsNull(target);
+        }
+
+
+        /* ====== Tests for the correct targets ====== */
+
+        /*
+         * The following tests verify that the correct target is returned.
+         */
+
+        [Test]
+        public void CanGetRootTarget()
+        {
+            var root = routes.GetRouteInvocation(new VirtualPath("~/"));
+            Assert.AreSame(rootTarget, root.Target);
+        }
+
+        [Test]
+        public void CanGetOneTwoTarget()
+        {
+            var invocation = routes.GetRouteInvocation(new VirtualPath("~/one/two"));
+            Assert.AreSame(oneTwo, invocation.Target);
+        }
+
+        [Test]
+        public void CanGetParametrisedTarget()
+        {
+            var invocation = routes.GetRouteInvocation(new VirtualPath("~/one/two/three/4"));
+            Assert.AreSame(withId, invocation.Target);
+        }
+    }
+}
---
+++ src/Dolstagis.Web/Routes/IRouteTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dolstagis.Web.Http;
+
+namespace Dolstagis.Web.Routes
+{
+    public interface IRouteTable
+    {
+        /// <summary>
+        ///  Registers a route on this route table.
+        /// </summary>
+        /// <param name="specification">
+        ///  The route specification. null or an empty string indicates the root
+        ///  node.
+        /// </param>
+        /// <param name="target">
+        ///  The route target.
+        /// </param>
+        /// <param name="constraint">
+        ///  A predicate which allows us to add constraints to the route.
+        /// </param>
+
+        void Add(string specification, IRouteTarget target, Predicate<IRequest> constraint);
+
+        /// <summary>
+        ///  Gets the route target for the requested path.
+        /// </summary>
+        /// <param name="path">
+        ///  The path of the request.
+        /// </param>
+        /// <returns>
+        ///  The route target.
+        /// </returns>
+
+        RouteInvocation GetRouteInvocation(VirtualPath path);
+    }
+}
