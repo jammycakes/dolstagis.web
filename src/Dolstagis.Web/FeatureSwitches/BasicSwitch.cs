7084e09d05283ee1fc5d438c9f11205640aabf2e
James McKay <code@jamesmckay.net>
2014-09-15 21:15:18 +0100

    AppSettingsFeatureSwitch -> AppSettingsSwitchableAttribute

---
+++ src/Dolstagis.Web/FeatureSwitches/BasicSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolstagis.Web.FeatureSwitches
+{
+    public class BasicSwitch : IFeatureSwitch
+    {
+        private bool _enabled;
+
+        public Task<bool> IsEnabledForRequest(Http.IRequest request)
+        {
+            return Task.FromResult(_enabled);
+        }
+
+        public bool DependentOnRequest { get { return false;} }
+
+        public BasicSwitch(bool enabled)
+        {
+            _enabled = enabled;
+        }
+    }
+}
