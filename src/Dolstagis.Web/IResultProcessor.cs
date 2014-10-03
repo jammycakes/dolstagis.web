d2141ff495410003bd42ff2abd86f0663bc427b5
James McKay <code@jamesmckay.net>
2014-10-03 07:57:28 +0100

    Move IResultProcessor to root namespace. It's part of the core API.

--- src/Dolstagis.Web/IResultProcessor.cs
+++ src/Dolstagis.Web/IResultProcessor.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Dolstagis.Web.Http;
 
-namespace Dolstagis.Web.Lifecycle
+namespace Dolstagis.Web
 {
     public interface IResultProcessor
     {
