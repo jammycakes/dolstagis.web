7de96db60f4e624eea4bfaf050e294b5ff6c8e9f
James McKay <code@jamesmckay.net>
2016-02-02 22:59:08 +0000

    Make feature switches and top-level IOC configuration mutually exclusive.
    This restriction is to avoid confusion and potential bugs. Top-level IOC configuration (with `Setup.Application(c = >...)`) is always available to the application regardless of whether the feature is switched on or off. If we were to allow switchable features to specify application-level services, we could easily end up with code being switched on when it is believed to be switched off.

---
+++ src/Dolstagis.Tests/Web/Features/FluentConfiguration/ConfigConflictFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Dolstagis.Tests.Web.Features.FluentConfiguration
+{
+    [TestFixture]
+    public class ConfigConflictFixture
+    {
+        [Test]
+        public void AddingGlobalConfigurationToFeatureSwitchShouldThrow()
+        {
+            Assert.Throws<InvalidOperationException>(() => {
+                new ConfigConflictFeature(true, true, false);
+            });
+        }
+
+
+        [Test]
+        public void AddingFeatureSwitchToGlobalConfigurationShouldThrow()
+        {
+            Assert.Throws<InvalidOperationException>(() => {
+                new ConfigConflictFeature(false, true, true);
+            });
+        }
+    }
+}
--- src/Dolstagis.Web/Features/IContainerBuilder.cs
+++ src/Dolstagis.Web/Features/IContainerBuilder.cs
@@ -7,9 +7,13 @@ namespace Dolstagis.Web.Features
         bool HasInstance { get; }
         IIoCContainer Instance { get; }
         Type ContainerType { get; }
+        bool ApplicationLevel { get; }
         IIoCContainer CreateContainer();
         void SetupApplication(IIoCContainer container);
         void SetupDomain(IIoCContainer container);
         void SetupRequest(IIoCContainer container);
+
+        event EventHandler ConfiguringApplication;
+        event EventHandler SettingContainer;
     }
 }
