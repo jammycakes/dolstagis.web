edc7841aa1d5f35b12f63c90e1d0b14e1eda5224
James McKay <code@jamesmckay.net>
2016-02-05 07:25:28 +0000

    New expression-based approach to creating route targets.

---
+++ src/Dolstagis.Tests/Web/Routes/RouteTargetFixture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dolstagis.Web.Routes;
+using Moq;
+using NUnit.Framework;
+
+namespace Dolstagis.Tests.Web.Routes
+{
+    [TestFixture]
+    public class RouteTargetFixture
+    {
+        [Test]
+        public void CanCreateRouteTargetFromType()
+        {
+            object obj = new object();
+
+            var mockServiceProvider = new Mock<IServiceProvider>();
+            mockServiceProvider.Setup(x => x.GetService(typeof(object))).Returns(obj);
+
+            Type t = typeof(object);
+
+            var target = new RouteTarget(x => x.GetService(typeof(object)));
+
+            Assert.AreEqual(typeof(object), target.ControllerType);
+            Assert.AreSame(obj, target.GetController(mockServiceProvider.Object));
+        }
+    }
+}
--- src/Dolstagis.Web/Routes/RouteTarget.cs
+++ src/Dolstagis.Web/Routes/RouteTarget.cs
@@ -1,14 +1,44 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace Dolstagis.Web.Routes
 {
     public class RouteTarget : IRouteTarget
     {
+        private Expression<Func<IServiceProvider, object>> _controllerExpr;
+        private Func<IServiceProvider, object> _controllerFunc;
+
+        private Expression<Func<IServiceProvider, IModelBinder>> _modelBinderExpr;
+
+        public object GetController(IServiceProvider provider)
+        {
+            return _controllerFunc(provider);
+        }
+
         public Type ControllerType { get; set; }
 
         public RouteTarget(Type controllerType)
         {
             ControllerType = controllerType;
+
+            var method = typeof(IServiceProvider).GetMethod("GetService", new Type[] { typeof(Type) });
+            var serviceProviderParam = Expression.Parameter(typeof(IServiceProvider), "provider");
+            var typeParam = Expression.Constant(controllerType, typeof(Type));
+            var resultParam = Expression.Parameter(typeof(object), "result");
+            var methodCallExpr = Expression.Call(serviceProviderParam, method, typeParam);
+
+            _controllerExpr =
+                Expression.Lambda<Func<IServiceProvider, object>>(methodCallExpr, serviceProviderParam);
+            _controllerFunc = _controllerExpr.Compile();
+        }
+
+        public RouteTarget(Expression<Func<IServiceProvider, object>> controllerExpr)
+        {
+            _controllerExpr = controllerExpr;
+            _controllerFunc = _controllerExpr.Compile();
+
+            ControllerType = ((_controllerExpr.Body as MethodCallExpression)?.Arguments?[0]
+                as ConstantExpression)?.Value as Type;
         }
     }
 }
