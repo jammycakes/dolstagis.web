79ab4251d56acc4b4fbf775d11e71b5af9eb48f8
James McKay <code@jamesmckay.net>
2016-02-13 19:07:13 +0000

    Strip pointless invocation exception added by MethodInfo.Invoke.

---
+++ src/Dolstagis.Web/Features/Impl/IFeatureSet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Dolstagis.Web.Http;
+using Dolstagis.Web.IoC;
+using Dolstagis.Web.Lifecycle;
+using Dolstagis.Web.Routes;
+
+namespace Dolstagis.Web.Features.Impl
+{
+    public interface IFeatureSet
+    {
+        Application Application { get; }
+        IIoCContainer Container { get; }
+        IReadOnlyCollection<IFeature> Features { get; }
+
+        RequestProcessor GetRequestProcessor();
+        RouteInvocation GetRouteInvocation(IRequest request);
+    }
+}
