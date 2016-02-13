005667d85666d26ea4043ef7f06179693f62e9bf
James McKay <code@jamesmckay.net>
2016-02-13 11:47:59 +0000

    XML-ish results from the view are always XML.
    XmlResult also supports custom serialisers and XSLT transforms.

--- src/Dolstagis.Web/ContentNegotiation/Types/XmlNegotiator.cs
+++ src/Dolstagis.Web/ContentNegotiation/Types/XmlNegotiator.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using Dolstagis.Web.Http;
 
 namespace Dolstagis.Web.ContentNegotiation.Types
@@ -19,6 +22,10 @@ namespace Dolstagis.Web.ContentNegotiation.Types
 
         public Negotiation Negotiate(IRequest request, object model)
         {
+            if (model is XDocument || model is IXPathNavigable || model is XPathNavigator) {
+                return new Negotiation(Match.Exact, 1.0, this, () => new XmlResult(model));
+            }
+
             Match match;
 
             var opt = request.Headers.MatchAccept(reIsXml);
--- src/Dolstagis.Web/XmlResult.cs
+++ src/Dolstagis.Web/XmlResult.cs
@@ -4,7 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
 using System.Xml.Serialization;
+using System.Xml.XPath;
+using System.Xml.Xsl;
 
 namespace Dolstagis.Web
 {
@@ -14,6 +18,10 @@ namespace Dolstagis.Web
 
         public XmlSerializer Serializer { get; set; }
 
+        public XslCompiledTransform Xslt { get; set; }
+
+        public XsltArgumentList XsltArgs { get; set; }
+
         public XmlResult(object model)
         {
             Model = model;
@@ -21,12 +29,51 @@ namespace Dolstagis.Web
             Encoding = System.Text.Encoding.UTF8;
         }
 
+        public override Task RenderAsync(IRequestContext context)
+        {
+            Encoding = Encoding ?? Encoding.UTF8;
+            return base.RenderAsync(context);
+        }
+
+        private async Task TransformAsync(IRequestContext context, XPathNavigator xml)
+        {
+            await Task.Run(() => {
+                using (var writer = new StreamWriter(context.Response.Body, Encoding))
+                using (var xWriter = new XmlTextWriter(writer)) {
+                    if (Xslt != null) {
+                        Xslt.Transform(xml, XsltArgs ?? new XsltArgumentList(), xWriter);
+                    }
+                    else {
+                        xml.WriteSubtree(xWriter);
+                    }
+                }
+            });
+        }
+
         protected override async Task SendBodyAsync(IRequestContext context)
         {
-            var ser = Serializer ?? new XmlSerializer(Model.GetType());
-            using (var writer = new StreamWriter
-                (context.Response.Body, Encoding ?? Encoding.UTF8)) {
-                await Task.Run(() => ser.Serialize(writer, Model));
+            if (Model is XDocument) {
+                await TransformAsync(context, ((XDocument)Model).CreateNavigator());
+            }
+            else if (Model is IXPathNavigable) {
+                await TransformAsync(context, ((IXPathNavigable)Model).CreateNavigator());
+            }
+            else if (Model is XPathNavigator) {
+                await TransformAsync(context, (XPathNavigator)Model);
+            }
+            else {
+                var ser = Serializer ?? new XmlSerializer(Model.GetType());
+                if (Xslt != null) {
+                    var doc = new XDocument();
+                    using (var writer = doc.CreateWriter())
+                        ser.Serialize(writer, Model);
+                    await TransformAsync(context, doc.CreateNavigator());
+                }
+                else {
+                    using (var writer = new StreamWriter(context.Response.Body, Encoding)) {
+                        ser.Serialize(writer, Model);
+                    }
+                }
             }
         }
     }
